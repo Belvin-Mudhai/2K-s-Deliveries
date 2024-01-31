6b0a3665cd347a604122bcb663beef344a01eef4
Belvin Mudhai <belvin.bm@gmail.com>
2024-01-31 13:58:22 +0300

    new delivery created and loaded

---
+++ Deliveries.sql
@@ -0,0 +1,14 @@
+USE delivery;
+CREATE TABLE deliveries(
+	Customers VARCHAR(8),
+    Delivery_date DATETIME,
+    Order_date DATETIME,
+    SN VARCHAR(8),
+    Warehouse VARCHAR(14),
+    Zipcode VARCHAR(8)
+);
+INSERT INTO deliveries(
+
+)
+
+INSERT INTO delivery.delivery_dataset(`S/N`,`Warehouse`,`Order date`,`Delivery date`,`Customer`,`Zipcode`) VALUES(?,?,?,?,?,?)'
