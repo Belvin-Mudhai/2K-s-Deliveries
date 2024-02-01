b38b8d7b7ac7a49ab7c3cdba8de804a11d45ff4a
Belvin Mudhai <belvin.bm@gmail.com>
2024-02-01 09:36:09 +0300

    Attempting to commit missed changes in sql file.

--- new_delivery.sql
+++ new_delivery.sql
@@ -10,11 +10,18 @@ CREATE TABLE deliveries (
             Warehouse VARCHAR(24),
             Zipcode VARCHAR(8)
             );
-drop table deliveries;
+drop table delivery_copy;
 use delivery_db;
 
 # After loading the data, the analysis begins
 select * from delivery_copy limit 10;
+
+/* There is need to change the data types of the date columns to ensure consistency as some of 
+the entries were not really in the datetime type*/
+ALTER TABLE delivery_copy
+MODIFY COLUMN `Order date` datetime;
+
+
 
 # 1. Zip code with most orders --> 15241 with 919 Orders
 SELECT Zipcode, COUNT(Zipcode) AS The_Counts
@@ -32,3 +39,15 @@ SELECT COUNT(Customer) - COUNT(DISTINCT Customer) AS Number_of_returnees
 FROM delivery_copy;
 # Ratio is therefore 1:4.4
 
+# 3. Average delivery time for one-timers vs returning customers
+# change the order date to turn off the confusion
+ALTER TABLE delivery_copy
+CHANGE `Order date` Order_date DATETIME;
+
+
+/* Getting the differences in time --> unix_timestamp() 
+										timestampdiff()*/
+SELECT Warehouse, timestampdiff(hour, `Order date`, `Delivery date`) AS time_to_delivery,
+	Customer, Zipcode
+FROM delivery_copy
+Limit 10;
